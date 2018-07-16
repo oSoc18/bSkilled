58ac2ce02abac6da39dc5c1270d197bc1992e776
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-07-16 16:26:00 +0200

    search page with results

--- frontend/src/Search.vue
+++ frontend/src/Search.vue
@@ -1,44 +1,47 @@
 <template>
-<div class="container">
-  <input type="text"   v-model="query"/>
-  <badge img="http://icons.iconarchive.com/icons/icons8/windows-8/512/City-Police-Badge-icon.png"  name="Super Awesome Badge" description="just another badge"></badge>
-  
-</div>
+  <div class="container">
+    <input type="text"   v-model="query"/>
+    <ul>
+      <li v-for="foundbadge in foundbadges" v-bind:key="foundbadge.id">
+          <badge :img="foundbadge.image"  :name="foundbadge.name" :description="foundbadge.description"></badge>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-  import Vue from 'vue';
-  import BadgeClassTemp from './BadgeClassTemp.vue';
-  Vue.component('badge', BadgeClassTemp);
+import Vue from "vue";
+import BadgeClassTemp from "./BadgeClassTemp.vue";
+Vue.component("badge", BadgeClassTemp);
 
-  
-  export default {
-    data() {
-      return {
-        query:""
-      };
-    },
-    watch: {
-      query: function(val, oldVal) {
-      // change of userinput, do something
-      console.log();
-      let testdata ={ "ogh5txW7I": {
-        "id": "http://localhost/badgeClassA.json",
-        "type": "BadgeClass",
-        "name": "Awesome Example Badge",
-        "description": "For doing awesome.",
-        "image": "http://icons.iconarchive.com/icons/seanau/fresh-web/512/Badge-icon.png"
-      }};
-      let response = {};
-      let querry = "Searched";
-        for(var key in testdata) {
-            if(testdata.hasOwnProperty(key)) {
-                if (testdata[key].name.str.search(querry)>=0){
-                  response[key] = testdata[key];
-                }
-            }
+export default {
+  data() {
+    return {
+      query: "",
+      badgeclasses: {
+        ogh5txW7I: {
+          id: "http://localhost/badgeClassA.json",
+          type: "BadgeClass",
+          name: "Awesome Example Badge",
+          description: "For doing awesome.",
+          image: "http://icons.iconarchive.com/icons/icons8/windows-8/512/City-Police-Badge-icon.png"
+        }
+      },
+      foundbadges: {
+      }
+    };
+  },
+  watch: {
+    query: function(val, oldVal) {
+      this.foundbadges = {};
+      for (var key in this.badgeclasses) {
+        if (this.badgeclasses.hasOwnProperty(key)) {
+          if (this.badgeclasses[key].name.search(val) >= 0) {
+            this.foundbadges[key] = this.badgeclasses[key];
+          }
         }
       }
     }
   }
+};
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
 import store from './store.js';
 
 import Landing from './Landing.vue';
