24bf22304db2f031b781087ab8db38660923c791
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-07-17 11:37:20 +0200

    keygen function

--- frontend/src/components/Sign/Generate/Generate.vue
+++ frontend/src/components/Sign/Generate/Generate.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="form_generate-key">
-    <v-header>Let's get a personal key</v-header>
+    <v-header>Let's get a profile key</v-header>
     <form v-if="!submitted">
       <label>First name</label>
-      <input type="text" v-model.lazy="key.firstName"  />
+      <input type="text" v-model.lazy="profile.firstName"  />
       <label>Last name</label>
-      <input type="text" v-model.lazy="key.lastName" />
+      <input type="text" v-model.lazy="profile.lastName" />
       <label>E-mail adress</label>
-      <input type="text" v-model.lazy="key.email" />
+      <input type="text" v-model.lazy="profile.email" />
       <label>Company name</label>
-      <input type="text" v-model.lazy="key.company" required />
+      <input type="text" v-model.lazy="profile.company" required />
       <label>Website url</label>
-      <input type="text" v-model.lazy="key.url" required />
+      <input type="text" v-model.lazy="profile.url" required />
       <button v-on:click.prevent="post">Get your personal key</button>
     </form>
     <div v-if="submitted">
@@ -21,8 +21,9 @@
 </template>
 
 <script>
-  import Header from 'Components/Header'
-  import Button from 'Components/Button'
+  import Header from 'Components/Header';
+  import Button from 'Components/Button';
+  import kbpgp from 'kbpgp';
 
   export default {
     name: 'app',
@@ -32,7 +33,7 @@
     },
     data () {
         return {
-            key: {
+            profile: {
                 firstName: '',
                 lastName: '',
                 email: '',
@@ -45,14 +46,56 @@
     },
     methods: {
         post: function(){
+            console.log("generating");
+            this.generate();
+            /*
             this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-                title: this.key.firstName,
-                body: this.key.lastName,
+                title: this.profile.firstName,
+                body: this.profile.lastName,
                 userId: 1
             }).then(function(data){
                 console.log(data);
                 this.submitted = true;
-            });
+            });*/
+        },
+        generate: function(useridentifier,pass){
+          var F = kbpgp["const"].openpgp;
+          var opts = {
+            userid: useridentifier,
+            primary: {
+              nbits: 2048,
+              flags: F.certify_keys | F.sign_data | F.auth,
+              expire_in: 0  // never expire
+            },
+            subkeys: [
+              {
+                nbits: 2048,
+                flags: F.sign_data,
+                expire_in: 0
+              }
+            ]
+          };
+
+          kbpgp.KeyManager.generate(opts, function(err, theKeyManager) {
+            if (!err) {
+              // sign alice's subkeys
+              theKeyManager.sign({}, function(err) {
+                console.log(theKeyManager);
+                // export demo; dump the private with a passphrase
+                theKeyManager.export_pgp_private ({
+                  passphrase: pass
+                }, function(err, pgp_private) {
+                  console.log("private key: ", pgp_private);
+                });
+                theKeyManager.export_pgp_public({}, function(err, pgp_public) {
+                  console.log("public key: ", pgp_public);
+                });
+              });
+            }
+            else{
+              console.log('error' + err);
+            }
+          });
         }
     }
   }
