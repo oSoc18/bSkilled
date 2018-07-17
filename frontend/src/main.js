35ad6e5f47b73045935b36a18bb65909a5cce610
Wout Schellaert <wout.schellaert@gmail.com>
2018-07-17 11:12:21 +0200

    Fix some flow issues

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,38 +1,43 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+
 import store from './store.js';
 
 import Landing from './Landing.vue';
 import Recipient from './Recipient.vue';
 import Share from './Share.vue';
 import App from './App.vue';
-import Search from './Search.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-const routes = [
-  { path: '/', name: 'landing', component: Landing },
+// Little helper for modeling the flow in the pages
+const flow = (pred, succ, redirect = '/') => (to, from, next) => {
+  if (from.name !== pred && from.name !== succ) {
+    return next(redirect);
+  }
+  return next();
+}
+
+const routes = [{
+    path: '/',
+    name: 'landing',
+    component: Landing,
+  },
   {
     path: '/recipient',
     name: 'recipient',
     component: Recipient,
     props: true,
-    beforeEnter: (to, from, next) => {
-      if (from.name != 'landing') { next('/'); }
-      next();
-    }
+    beforeEnter: flow('landing', 'share')
   },
   {
     path: '/share',
     name: 'share',
     component: Share,
     props: true,
-    beforeEnter: (to, from, next) => {
-      if (from.name != 'recipient') { next('/'); }
-      next();
-    }
+    beforeEnter: flow('recipient', undefined)
   },
 ];
 
