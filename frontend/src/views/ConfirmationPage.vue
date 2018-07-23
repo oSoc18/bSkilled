2a281a042d0d9d924e01c37ad7d82bf8852e8715
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-07-23 15:21:04 +0200

    img url

--- frontend/src/views/ConfirmationPage.vue
+++ frontend/src/views/ConfirmationPage.vue
@@ -5,7 +5,6 @@
     <p>{{implication.recipient}}</p>
     <p>Wan't to sign this badge?</p>
     <v-button :onClick="sign">Sign</v-button>
-    <input type="file" id="inputfied"  @change="testBake"/>
     <img src="" id="EndBadge" alt="">
   </div>
 </template>
@@ -16,15 +15,15 @@ import Header from "Components/TheHeader";
 import Button from "Components/Button";
 import BadgeClassCard from "Components/BadgeClassCard";
 
-import extract from 'png-chunks-extract';
-import encode from 'png-chunks-encode'; 
-import text from 'png-chunk-text';
+import extract from "png-chunks-extract";
+import encode from "png-chunks-encode";
+import text from "png-chunk-text";
 
 export default {
   data() {
     return {
       imgsrc: null
-    }
+    };
   },
   components: {
     "v-header": Header,
@@ -36,27 +35,43 @@ export default {
   },
   methods: {
     sign() {
+      let context = this;
+      context.testBake(context, "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png","thisisthejws.dotseperated.code")
       this.$store.dispatch("createSignedBadge", this.implication);
     },
-    testBake(ev) {
-      const file = ev.target.files[0];
-      const reader = new FileReader();
-      reader.onload = this.imgdataloaded;
-      reader.readAsArrayBuffer(file);
+    testBake(context, url, jws) {
+      try {
+        this.$http
+          .get(url, { responseType: "blob" })
+          .then(response => {
+            // resolve to Blob
+            return response.blob();
+          })
+          .then(blob => {
+            let imgFile = new File([blob], "badgeimage.png");
+            const reader = new FileReader();
+            reader.onload = function(ev) {
+              let arrayBuffer = ev.target.result;
+              let buffer = Buffer.from(arrayBuffer);
+              context.bakeDataIntoImage(context, buffer, jws);
+            };
+            reader.readAsArrayBuffer(imgFile);
+          });
+      } catch (error) {
+        alert("There was an issue while baking: " + error);
+      }
     },
-    imgdataloaded(ev) {
-      //use ev.target.result
-      console.log(ev.target.result);
-      let arrayBuffer = ev.target.result;
-      let buffer = Buffer.from(arrayBuffer);
+    bakeDataIntoImage(context, buffer, jws) {
       console.log(buffer);
       var chunks = extract(buffer);
-      chunks.splice(-1, 0, text.encode("hello", "world"));
+      chunks.splice(-1, 0, text.encode("openbadges", jws));
       var newbuffer = new Buffer(encode(chunks));
       console.log(newbuffer);
       let base64Data = btoa(String.fromCharCode.apply(null, newbuffer));
       console.log(base64Data);
-      document.getElementById('EndBadge').src='data:image/png;base64,' + base64Data;
+      document.getElementById("EndBadge").src =
+        "data:image/png;base64," + base64Data;
+      //TODO do something with baked badge
     }
   }
 };
