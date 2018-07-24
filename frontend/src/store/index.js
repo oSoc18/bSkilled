23ce1d30d1adddc57cf67a62754c42a856030514
Wout Schellaert <wout.schellaert@gmail.com>
2018-07-24 11:31:09 +0200

    Connect signing

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import uuidv1 from 'uuid/v1';
 
+import jws from "jws";
 import forge from "node-forge";
 const pki = forge.pki;
 
@@ -15,10 +16,17 @@ const state = {
   // sharing: search, recipient, share
   // signing: sign, upload, generate, profile, confirmation, signed
   currentFlowStep: "search",
+
+  // Sharing
   badgeTemplate: undefined,
+  recipient: undefined,
+
+  // Signing
   keyForge: undefined,
   implication: undefined,
   assertion: undefined,
+  signedAssertion: undefined,
+
   share: undefined
 };
 
@@ -36,6 +44,8 @@ const SAVE_IMPLICATION = "SAVE_IMPLICATION";
 const SAVE_KEY = "SAVE_KEY";
 const SAVE_PROFILE = "SAVE_PROFILE";
 const SAVE_ASSERTION = "SAVE_ASSERTION";
+const SAVE_SIGNED_ASSERTION = "SAVE_SIGNED_ASSERTION";
+const SAVE_BAKED_BADGE = "SAVE_BAKED_BADGE";
 
 
 const mutations = {
@@ -60,8 +70,8 @@ const mutations = {
   [SAVE_IMPLICATION](state, implication) {
     state.implication = implication;
   },
-  [SAVE_KEY](state, { keyForge, pubKey, fingerPrint }) {
-    state.key = { keyForge, pubKey, fingerPrint }
+  [SAVE_KEY](state, { keyForge, pem, fingerPrint }) {
+    state.key = { keyForge, pem, fingerPrint }
   },
   [SAVE_PROFILE](state, profile) {
     state.profile = profile;
@@ -69,6 +79,12 @@ const mutations = {
   [SAVE_ASSERTION](state, assertion) {
     state.assertion = assertion;
   },
+  [SAVE_SIGNED_ASSERTION](state, signedAssertion) {
+    state.signedAssertion = signedAssertion;
+  },
+  [SAVE_BAKED_BADGE](state, bakedBadge) {
+    state.bakedBadge = bakedBadge;
+  }
 };
 
 // TODO: Create action types
@@ -98,6 +114,10 @@ const actions = {
         'profile': (state) => ({
           nextFlowStep: 'confirm',
           nextRoute: { name: 'confirm' }
+        }),
+        'confirm': (state) => ({
+          nextFlowStep: 'download',
+          nextRoute: { name: 'download' }
         })
       }
     };
@@ -105,7 +125,7 @@ const actions = {
     commit(SET_CURRENT_FLOW_STEP, next.nextFlowStep);
     router.push(next.nextRoute);
   },
-  // Submit
+  // Sharing
   createImplication({ commit, state }, recipient) {
     console.log(`Submitting badge for ${recipient}`);
     const badgeTemplate = state.badgeTemplate;
@@ -132,9 +152,9 @@ const actions = {
       keyForge.n,
       keyForge.e
     );
-    const pubKey = pki.privateKeyToPem(keyForge);
+    const pem = pki.privateKeyToPem(keyForge);
     const fingerprint = pki.getPublicKeyFingerprint(pubKeyForge);
-    commit(SAVE_KEY, { keyForge, pubKey, fingerprint });
+    commit(SAVE_KEY, { keyForge, pem, fingerprint });
 
     console.log("Niels fix me");
     // Vue.http.get(`${process.env.API}profile/`).then((resp) => {
@@ -147,45 +167,46 @@ const actions = {
     commit(SAVE_PROFILE, profile);
     // TODO: Post profile to backend
   },
-  createSignedBadge({ commit }, implication) {
-    console.log("Creating badge", implication);
-    const badgeClass = {
-      type: "BadgeClass",
-      id: implication.badgeTemplate.id,
-      name: implication.badgeTemplate.name,
-      description: implication.badgeTemplate.description,
-      image: implication.badgeTemplate.image,
-      criteria: implication.badgeTemplate.criteria,
-      issuer: {
-        type: "Profile",
-        id: implication.profile.id,
-        name: implication.profile.name,
-        url: implication.profile.url,
-        email: implication.profile.email,
-        publicKey: implication.profile.publicKey
-      }
-    }
+  signBadge({ dispatch, commit }) {
+    return Promise
+      .resolve(dispatch('createSignedBadge'))
+      .then((assertion) => {
+        console.log("Saving assertion", JSON.parse(JSON.stringify(assertion)));
+        commit(SAVE_ASSERTION, assertion);
+        const signature = jws.sign({
+          header: { alg: "RS256" },
+          privateKey: this.state.key.pem,
+          payload: assertion
+        });
+        commit(SAVE_SIGNED_ASSERTION, signature);
+        console.log(JSON.parse(JSON.stringify(signature)));
+      });
+  },
+  bakeBadge({ commit, state }) {
+    console.log("TODO");
+    commit(SAVE_BAKED_BADGE, state.bakedBadge);
+  },
+  createSignedBadge() {
+    const implication = state.implication;
+    const badgeTemplate = implication.badgeTemplate;
+    const profile = state.profile;
 
-    const assertion = {
+    const issuer = { type: 'Profile', ...profile };
+    const badge = { type: "BadgeClass", ...badgeTemplate, issuer };
+    const recipient = { type: 'email', hashed: false, identity: implication.recipient };
+    // TODO: Check badge compliance
+    const verification = { type: 'SignedBadge', creator: profile.publicKey };
+    const issuedOn = new Date(Date.now()).toISOString();
+
+    return {
       "@context": "https://w3id.org/openbadges/v2",
       type: "Assertion",
       id: `urn:uuid:${uuidv1()}`,
-      badge: badgeClass,
-      recipient: {
-        type: "email",
-        hashed: false,
-        identity: implication.recipient
-      },
-      issuedOn: new Date(Date.now()).toISOString(),
-      verification: {
-        type: "SignedBadge",
-        creator: implication.profile.publicKey
-      }
-    }
-
-    commit(SAVE_ASSERTION, assertion);
-    console.log("Created badge:", assertion);
-    router.push({ name: "signed" });
+      badge,
+      recipient,
+      issuedOn,
+      verification,
+    };
   }
 };
 
--- frontend/src/views/ConfirmationPage.vue
+++ frontend/src/views/ConfirmationPage.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="app">
     <BadgeClassCard :badge-class="implication.badgeTemplate" />
-    <!-- <p>profile: {{implication.profile}}</p> -->
     <p>{{implication.recipient}}</p>
     <p>Wan't to sign this badge?</p>
     <v-button :onClick="sign">Sign</v-button>
-    <input type="file" id="inputfied"  @change="testBake"/>
-    <img src="" id="EndBadge" alt="">
+    <v-button :onClick="dontSign">Don't sign</v-button>
   </div>
 </template>
 
@@ -16,18 +14,17 @@ import Header from "Components/TheHeader";
 import Button from "Components/Button";
 import BadgeClassCard from "Components/BadgeClassCard";
 
-import extract from 'png-chunks-extract';
-import encode from 'png-chunks-encode'; 
-import text from 'png-chunk-text';
+import extract from "png-chunks-extract";
+import encode from "png-chunks-encode";
+import text from "png-chunk-text";
 
 export default {
   data() {
     return {
-      imgsrc: null
-    }
+      flowStep: "confirmation"
+    };
   },
   components: {
-    "v-header": Header,
     "v-button": Button,
     BadgeClassCard
   },
@@ -36,28 +33,39 @@ export default {
   },
   methods: {
     sign() {
-      this.$store.dispatch("createSignedBadge", this.implication);
+      this.$store
+        .dispatch("signBadge")
+        .then(() => this.$store.dispatch("bakeBadge"))
+        .then(() => this.$store.dispatch("stepFlow"))
+        .catch(err => console.log(err));
     },
-    testBake(ev) {
-      const file = ev.target.files[0];
-      const reader = new FileReader();
-      reader.onload = this.imgdataloaded;
-      reader.readAsArrayBuffer(file);
-    },
-    imgdataloaded(ev) {
-      //use ev.target.result
-      console.log(ev.target.result);
-      let arrayBuffer = ev.target.result;
-      let buffer = Buffer.from(arrayBuffer);
-      console.log(buffer);
-      var chunks = extract(buffer);
-      chunks.splice(-1, 0, text.encode("hello", "world"));
-      var newbuffer = new Buffer(encode(chunks));
-      console.log(newbuffer);
-      let base64Data = btoa(String.fromCharCode.apply(null, newbuffer));
-      console.log(base64Data);
-      document.getElementById('EndBadge').src='data:image/png;base64,' + base64Data;
+    dontSign() {
+      alert("Don't sign!");
     }
+    // testBake(ev) {
+    //   const file = ev.target.files[0];
+    //   const reader = new FileReader();
+    //   reader.onload = this.imgdataloaded;
+    //   reader.readAsArrayBuffer(file);
+    // },
+    // imgdataloaded(ev) {
+    //   //use ev.target.result
+    //   console.log(ev.target.result);
+    //   let arrayBuffer = ev.target.result;
+    //   let buffer = Buffer.from(arrayBuffer);
+    //   console.log(buffer);
+    //   var chunks = extract(buffer);
+    //   chunks.splice(-1, 0, text.encode("hello", "world"));
+    //   var newbuffer = new Buffer(encode(chunks));
+    //   console.log(newbuffer);
+    //   let base64Data = btoa(String.fromCharCode.apply(null, newbuffer));
+    //   console.log(base64Data);
+    //   document.getElementById("EndBadge").src =
+    //     "data:image/png;base64," + base64Data;
+    // }
+  },
+  activated() {
+    this.$store.commit("SET_CURRENT_FLOW_STEP", this.flowStep);
   }
 };
 </script>
